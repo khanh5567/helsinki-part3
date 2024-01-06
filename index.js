9e2397b4af7e234a5c89d06997a7369283da6184
Jimmy <tranvanhongkhanh@gmail.com>
2024-01-06 00:19:28 -0700

    3.8 complete

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+var morgan = require("morgan");
 const app = express();
 app.use(express.json());
 
@@ -24,6 +25,25 @@ let data = [
     number: "39-23-6423122",
   },
 ];
+
+morgan.token("body", function (req, res) {
+  return JSON.stringify(req.body);
+});
+
+app.use(
+  morgan((tokens, req, res) => {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, "content-length"),
+      "-",
+      tokens["response-time"](req, res),
+      "ms",
+      tokens.body(req, res),
+    ].join(" ");
+  })
+);
 
 app.get("/api/persons", (req, res) => {
   res.json(data);
