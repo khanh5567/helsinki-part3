f6c2d12377affb3df8c2b4448e69ca98ac772b8d
Jimmy <tranvanhongkhanh@gmail.com>
2024-01-10 23:19:47 -0700

    3.12 complete

---
+++ mongo.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const arg_len = process.argv.length;
+if (arg_len < 3) {
+  console.log("provide password");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://jimmy:${password}@cluster0.b4jqvyu.mongodb.net/noteApp?retryWrites=true&w=majority`;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+if (arg_len === 3) {
+  Person.find({}).then((result) => {
+    console.log("phonebook");
+    result.forEach((person) => console.log(person.name + " " + person.number));
+    mongoose.connection.close();
+  });
+} else if (arg_len !== 5) {
+  console.log("give name and number as arguments");
+  process.exit(1);
+} else {
+  const name = process.argv[3];
+  const number = process.argv[4];
+
+  const person = new Person({
+    name: name,
+    number: number,
+  });
+
+  person.save().then((result) => {
+    console.log(`added ${name} number ${number} to phonebook`);
+    mongoose.connection.close();
+  });
+}
